6f76b3ec8ebf16675658775485c7e27eb90dad1c
sushanth4113 <111417924+sushanth4113@users.noreply.github.com>
2022-08-18 13:38:52 +0530

    Update celcious to farhanite.c

--- celcious to farhanite.c
+++ celcious to farhanite.c
@@ -1,15 +1,13 @@
 #include<stdio.h>
 #include<conio.h>
 float celToFahFun(float);
-int main()
+Void main()
 {
     float Celsius, Fahrenheit;
     printf("Enter Temperature Value (in Celsius): ");
     scanf("%f", &Celsius);
     Fahrenheit = celToFahFun(Celsius);
     printf("\nEquivalent Temperature Value (in Fahrenheit) = %0.2f", Fahrenheit);
-    getch();
-    return 0;
 }
 float celToFahFun(float cel)
 {
